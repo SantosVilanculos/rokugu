06d7f6524dc7375a4c59865090e0266b014f0c22
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-18 22:10:30 +0200

    build: use README.md contents has long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+from pathlib import Path
+
 from setuptools import setup
 
 setup(
     name="rokugu",
     version="0.0.1",
-    long_description="An opinionated PyQt/PySide6 component library offering a collection of aesthetically pleasing and highly accessible UI components.",
-    readme="./README.md",
     description="An opinionated PySide6 library that delivers ready-to-use components and utilities.",
+    long_description=Path("README.md").read_text("UTF-8"),
+    long_description_content_type="text/markdown",
     author="Santos Vilanculos",
     author_email="santosvilanculos@yahoo.com",
     url="http://github.com/SantosVilanculos/rokugu",
