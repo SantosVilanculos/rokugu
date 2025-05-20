086c9a154c82223cd87d0dd1b480da000a8aa574
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-05-20 18:20:56 +0200

    chore: add examples app release workflow

--- examples/utils.py
+++ examples/utils.py
@@ -5,6 +5,6 @@ from typing import Union
 
 def asset(path: Union[Path, str] = "") -> Path:
     if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
-        return Path(sys._MEIPASS).joinpath(path)
+        return Path(sys._MEIPASS).joinpath("assets", path)
 
     return Path().cwd().joinpath("examples", "assets", path)
